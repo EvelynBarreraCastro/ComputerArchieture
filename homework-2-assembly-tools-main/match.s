3485fdaed022ba9c894bf39695b6e8e794aac4a6
evelyn barrera castro <e.barreracastro@wsu.edu>
2025-11-30 17:38:05 -0800

    Update match.s

--- homework-2-assembly-tools-main/match.s
+++ homework-2-assembly-tools-main/match.s
@@ -1,23 +1,19 @@
-.globl match
+.globl match    #Assembler directive used to make globally visible to other files
+                #The linker uses this to find starting point of "match"# MIPS Assembly match function
 
-# MIPS Assembly match function
-#
+                
+# MIPS Assembly match function 
 # (inputs) $a0 is the size of the needle string
 # (inputs) $a1 is a pointer to the needle string
 # (inputs) $a2 is the size of the haystack string
 # (inputs) a3 is a pointer to the haystack string
 # (return value) $v0 is the index of the match, or -1 if no match
 
-# match: #placeholder for code (lines 11 -13)
-#   addi $v0, $zero, -2
-#   jr $ra
-
-
 # Note: Algorithm Plan (The core problem is a string search Algorithm)
 # A nested loop would be a good approach. 
 
 # Outline: 
-# Outer loop that iterates through the said "haystack string", one char at a time. 
+# Outer loop: that iterates through the said "haystack string", one char at a time. 
 # Inner loop: Given the position in the outer loop, us the inner loop to compare the
 # characters of the needle string to the characters in the haystack. 
 # As a result: If the 2 characters match, the loop continues to move on to the next
@@ -25,6 +21,7 @@
 # inner loop and return to the outer loop. 
 # Success Condition: If the inner loop successfully compares every character, we've found a match. 
 # Failure Condition: If the outer loop ends without finding a match -> needle does not exist in haystack. 
+
 
 match: 
     beq $a0, $zero, return_zero
