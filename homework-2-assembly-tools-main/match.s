d20ad5796dcda6bc74323d074dc2b1682678205f
evelyn barrera castro <e.barreracastro@wsu.edu>
2025-11-30 18:24:31 -0800

    Enhance match function with detailed comments
    Added comments to clarify the purpose of each function and instruction in the match function.

--- homework-2-assembly-tools-main/match.s
+++ homework-2-assembly-tools-main/match.s
@@ -23,47 +23,54 @@
 # Failure Condition: If the outer loop ends without finding a match -> needle does not exist in haystack. 
 
 
+# match function created. 
 match: 
-    beq $a0, $zero, return_zero
-    blt $a2, $a0, return_neg1
+    beq $a0, $zero, return_zero      # branch to "return_zero" if $a0 == $zero ... else continue instructions 
+    blt $a2, $a0, return_neg1        # branch to "return_neg1" if $a2 < $a0 ... else continue instructions
 
-    move $t0, $zero
+    move $t0, $zero                  # moves $zero value into $t0 giving us ---> $t0 = 0
 
+#outer_loop function created. 
 outer_loop: 
-    sub $t1, $a2, $a0
-    bgt $t0, $t1, return_neg1
+    sub $t1, $a2, $a0                # $t1 = $a2 - $a0
+    bgt $t0, $t1, return_neg1        # branch to "return_neg1" if $t0 > $t1
 
-    move $t2, $zero
+    move $t2, $zero                  # $t2 = 0 (initialize character index) 
 
+#inner_loop function created. 
 inner_loop: 
-    beq $t2, $a0, found_match
+    beq $t2, $a0, found_match        # branch to "found_match" if $t2 == $a0
 
-    add $t3, $t0, $t2
-    add $t3, $t3, $a3
-    lbu $t4, 0($t3)
+    add $t3, $t0, $t2                # $t3 = $t0 + $t2
+    add $t3, $t3, $a3                # $t3 = $t3 + $a3 
+    lbu $t4, 0($t3)                  # Load byte unsigned --> calculates address of $t3 and moves it to $t4
 
-    add $t5, $a1, $t2
-    lbu $t6, 0($t5)
+    add $t5, $a1, $t2                # $t5 = $a1 + $t2
+    lbu $t6, 0($t5)                  # $t6 = loads address of $t5 & stores it 
 
-    bne $t4, $t6, not_match
+    bne $t4, $t6, not_match          # branch if not equal to "not_match" ---> if $t4 != $t6
 
-    addi $t2, $t2, 1
-    j inner_loop
+    addi $t2, $t2, 1                 # $t2 = $t2 + 1
+    j inner_loop                     # jump to inner_loop function (repeat) 
 
-not_match: 
-  addi $t0, $t0, 1
-  j outer_loop
+#not_match function created. 
+not_match:                           
+  addi $t0, $t0, 1                   # $t0 = $t0 + 1
+  j outer_loop                       # jump to outer_loop function 
 
+#found_match function created. 
 found_match: 
-  move $v0, $t0 # return index of match
-  jr $ra
+  move $v0, $t0                     # $v0 = $t0  (return index of match position)  jr $ra                            
+  jr $ra                            # exit function and return argument in $v0 
 
+#return_neg1 function created. 
 return_neg1: 
-  addi $t0, $zero, -1
-  jr $ra
+  li $v0, -1                       # $v0 = -1 (return value) 
+  jr $ra                           # exit function & return argument of ($v0 = -1)
 
+#return_zero function created. 
 return_zero: 
-  add $v0, $zero, $zero 
-  jr $ra
+  add $v0, $zero, $zero           # $v0 = 0 + 0 = 0 
+  jr $ra                          # exit function & save return arguement of ($v0 = 0) 
 
 
